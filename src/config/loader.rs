65d18834fa4f16e07427b1cad7a569aece0b15e8
dipghoshraj <dipghoshraj@gmail.com>
2026-06-06 02:17:41 +0530

    using snakecase

--- src/config/loader.rs
+++ src/config/loader.rs
@@ -6,23 +6,23 @@ use std::fs;
 #[derive(Debug)]
 pub struct Node {
     // fields for the node
-    NodeAddress: String,
-    Status: String,
-    Region: String,
-    ID: String,
-    AnchorAddress: String,
+    node_address: String,
+    status: String,
+    region: String,
+    id: String,
+    anchor_address: String,
 }
 
 impl Node  {
     // methods for the node
-    fn LoadNodesFromFile(file_path: &str) -> Result<Self, Box<dyn std::error::Error>>  {
+    pub fn load_node_from_file(file_path: &str) -> Result<Self, Box<dyn std::error::Error>>  {
         let file_content = fs::read_to_string(file_path).expect("Failed to read file");
         
-        let mut NodeAddresses: String = String::new();
-        let mut Statuses: String = String::new();
-        let mut Regions: String = String::new();
-        let mut IDs: String = String::new();
-        let mut AnchorAddresses: String = String::new();
+        let mut node_address: String = String::new();
+        let mut status: String = String::new();
+        let mut region: String = String::new();
+        let mut id: String = String::new();
+        let mut anchor_address: String = String::new();
 
         for line in file_content.lines() {
             let line = line.trim();
@@ -36,16 +36,21 @@ impl Node  {
                 .ok_or("Invalid config line")?;
 
             match key.trim() {
-                "NodeAddress" => NodeAddresses = value.trim().to_string(),
-                "Status" => Statuses = value.trim().to_string(),
-                "Region" => Regions = value.trim().to_string(),
-                "ID" => IDs = value.trim().to_string(),
-                "AnchorAddress" => AnchorAddresses = value.trim().to_string(),
+                "NodeAddress" => node_address = value.trim().to_string(),
+                "Status" => status = value.trim().to_string(),
+                "Region" => region = value.trim().to_string(),
+                "ID" => id = value.trim().to_string(),
+                "AnchorAddress" => anchor_address = value.trim().to_string(),
                 _ => {}
             }
             
         }
-        Ok(Node { NodeAddress: NodeAddresses, Status: Statuses, Region: Regions, ID: IDs, AnchorAddress: AnchorAddresses })
+
+        if id.is_empty() {
+            return Err("ID is required create the identifier follow this steps".into());
+        }
+
+        Ok(Node { node_address, status, region, id, anchor_address })
 
         
     }
