40bbaf21cc3de9cb5f2ee756a8930c089666bb77
dipghoshraj <dipghoshraj@gmail.com>
2026-06-12 01:04:16 +0530

    added the encoder bytes

---
+++ transport/src/frame/encoder.rs
@@ -0,0 +1,20 @@
+use bytes::{BufMut, Bytes, BytesMut};
+use crate::frame;
+
+
+pub fn encode(frame: &frame::frame::Frame) -> Bytes {
+    let total_size = frame::header::HEADER_SIZE + frame.payload.len();
+
+    let mut buffer =  BytesMut::with_capacity(total_size);
+
+
+    buffer.extend_from_slice(&frame.header.magic);
+    buffer.put_u8(frame.header.version);
+    buffer.extend_from_slice(&frame.header.flags.to_be_bytes());
+    buffer.extend_from_slice(&frame.header.stream_id.to_be_bytes());
+    buffer.extend_from_slice(&frame.header.payload_length.to_be_bytes());
+    buffer.put_u8(frame.header.frame_type as u8);
+    buffer.extend_from_slice(&frame.payload);
+
+    buffer.freeze()
+}
